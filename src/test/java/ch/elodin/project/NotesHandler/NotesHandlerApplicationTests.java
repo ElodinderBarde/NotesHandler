628ffe04524481a7fde14c7ddb87cc86c28368bc
ElodinderBarde <r.marchese96@gmail.com>
2025-11-27 12:15:26 +0100

    Initaial commit, blank project with docker, lombock loaded

---
+++ src/test/java/ch/elodin/project/NotesHandler/NotesHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.elodin.project.NotesHandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
